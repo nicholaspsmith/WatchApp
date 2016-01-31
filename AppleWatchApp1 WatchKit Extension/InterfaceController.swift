875508f72067d173082111df6d7e5a205507bd88
Nicholas Smith <npsmith1990@gmail.com>
2016-01-31 14:35:08 -0600

    Make into heads or tails app

--- AppleWatchApp1 WatchKit Extension/InterfaceController.swift
+++ AppleWatchApp1 WatchKit Extension/InterfaceController.swift
@@ -12,21 +12,34 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet var nameLbl: WKInterfaceLabel!
+    
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
-        
-        // Configure interface objects here.
     }
 
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
-        NSLog("%@ init", self)
     }
 
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+    }
+    
+    @IBAction func buttonPressed() {
+        
+        let randomNumber = arc4random_uniform(2)
+        var label = ""
+        
+        if randomNumber == 0 {
+            label = "Heads"
+        } else {
+            label = "Tails"
+        }
+        
+        nameLbl.setText("\(label)")
     }
 
 }
